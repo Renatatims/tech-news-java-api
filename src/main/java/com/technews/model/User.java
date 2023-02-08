566709d2e9277f2e09a365b4275ba0dcb3ec4236
ReTims <renatatims@gmail.com>
2023-02-08 13:11:34 -0600

    Included annotations for the User's instance variables + Created Table Relationships for the User class + import jakarta.persistence.*

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -2,8 +2,7 @@ package com.technews.model;
 
 //Imports from annotations
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 //Class Level Annotations
 //User class can map to a table
@@ -15,11 +14,34 @@ import jakarta.persistence.Table;
 
 //Instance Variables in User Class
 public class User {
+    //Id annotations - Id unique identifier and generated value automatically
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String username;
+
+    //Email annotation - value must be unique (duplicates not allowed in the database)
+    @Column(unique = true)
     private String email;
     private String password;
+
+    //Logged In annotation - user's logged-in status won't persist in the data
+    @Transient
     boolean loggedIn;
+
+    //Table Relationships for the User Class
+
+    //Post list - fetch type eager: information will be gathered immediately after being created
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Post> posts;
+
+    // Vote List - Need to use FetchType.LAZY to resolve multiple bags exception
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Vote> votes;
+
+    // Comment List - Need to use FetchType.LAZY to resolve multiple bags exception
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> comments;
 
     //Instance Variables - Lists - collections of objects of the same type
     private List<Post> posts;
