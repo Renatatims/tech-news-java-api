e1b70f1c9d92dba2fc1dbcf5e5ade410f32715b0
ReTims <renatatims@gmail.com>
2023-02-08 14:43:00 -0600

    Included all the methods for Comment Java class - constructor, getters and setters, equals and hashCode and toString

--- src/main/java/com/technews/model/Comment.java
+++ src/main/java/com/technews/model/Comment.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 //Class-Level Annotations and Implementations
 @Entity
@@ -20,4 +21,71 @@ public class Comment implements Serializable {
     private Integer userId;
     private Integer postId;
 
+    //METHODS
+
+    //Constructor
+    public Comment(Integer id, String commentText, Integer userId, Integer postId) {
+        this.id = id;
+        this.commentText = commentText;
+        this.userId = userId;
+        this.postId = postId;
+    }
+
+    //getters and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    //equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment comment)) return false;
+        return Objects.equals(getId(), comment.getId()) && Objects.equals(getCommentText(), comment.getCommentText()) && Objects.equals(getUserId(), comment.getUserId()) && Objects.equals(getPostId(), comment.getPostId());
+    }
+
+    //hashCode
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getCommentText(), getUserId(), getPostId());
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", commentText='" + commentText + '\'' +
+                ", userId=" + userId +
+                ", postId=" + postId +
+                '}';
+    }
 }
