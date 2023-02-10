de328c4178ef62b24b87cee62e64c0f11e69416f
ReTims <renatatims@gmail.com>
2023-02-10 11:43:11 -0600

    HomePageController to define all repositories relationships - controller directory

---
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -0,0 +1,25 @@
+package com.technews.controller;
+
+import com.technews.repository.CommentRepository;
+import com.technews.repository.PostRepository;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+//Class-level annotation - Controller will control flow for the front-end
+@Controller
+public class HomePageController {
+
+    //Relationships for all the repositories - user, post, vote and comment
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+}
